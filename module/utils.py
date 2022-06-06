e6be3d2393c04ecca8fc878c4b582bd21b6b2141
weeswelai <52352067+weeswelai@users.noreply.github.com>
2022-06-06 14:25:18 +0800

    Create utils.py

---
+++ module/utils.py
@@ -0,0 +1,41 @@
+
+
+
+def deep_set(d:dict, keys:list or str, value):
+    """
+    from https://github.com/LmeSzinc/AzurLaneAutoScript/blob/master/module/config/utils.py
+    Set value into dictionary safely, imitating deep_get().
+    """
+    if isinstance(keys, str):
+        keys = keys.split('.')
+    assert type(keys) is list
+    if not keys:
+        return value
+    if not isinstance(d, dict):
+        d = {}
+    d[keys[0]] = deep_set(d.get(keys[0], {}), keys[1:], value)
+    return d
+
+def deep_get(d:dict, keys:list or str, default=None):
+    """
+    Get values in dictionary safely.
+    https://stackoverflow.com/questions/25833613/safe-method-to-get-value-of-nested-dictionary
+
+    Args:
+        d (dict):
+        keys (str, list): Such as `Scheduler.NextRun.value`
+        default: Default return if key not found.
+
+    Returns:
+
+    """
+    if isinstance(keys, str):
+        keys = keys.split('.')
+    assert type(keys) is list
+    if d is None:
+        return default
+    if not keys:
+        return d
+    return deep_get(d.get(keys[0]), keys[1:], default)
+
+
