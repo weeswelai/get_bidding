b04d3649c6b777f884bcf3a6dab54c986f7e1118
weeswelai <52352067+weeswelai@users.noreply.github.com>
2022-12-05 10:26:54 +0800

    test(string_test): refactor test code
    保存trie到本地可保存其他格式的文本文件
    删除从二进制文件中读取前缀树
    多行字符串转list中,转换一行后进行search_all

--- test/string_test.py
+++ test/string_test.py
@@ -25,13 +25,12 @@ str_t = """['吉林大学原子层沉积系统采购项目招标公告', '2022-1
 
 if __name__ == "__main__":
     ConsoleTest = 0  # 控制台调试,在代码最下方,需要添加断点
-    ReadFromStr = 1  # 测试字符串 存进前缀树
+    ReadFromStr = 0  # 测试字符串 存进前缀树
     LoadFromFile = 0  # 将文本文件的内容存进前缀树中
-    LoadFromFile_b = 0  # 从二进制文件读取前缀树
-    SaveLocalB = 0  # 保存前缀树到二进制文件中
-    Test_search_all = 0  # 测试检索整个字符串,返回符合的匹配项
+    SaveLocal = 0  # 保存前缀树到二进制文件中
+    Test_search_all = 1  # 测试检索整个字符串,返回符合的匹配项
 
-    # 将从word_insert 存进前缀树
+    # 将word 存进前缀树
     if ReadFromStr:
         word_insert = "声：光 测 试"
         title_trie.insert_from_str(word_insert)
@@ -41,30 +40,24 @@ if __name__ == "__main__":
         f_r = "./test/前缀树.txt"
         title_trie.init_from_file(f_r)
 
-    # 从二进制文件读取前缀树
-    if LoadFromFile_b:
-        title_trie.init_from_file_b()
-
-    # 保存前缀树到二进制文件中
-    if SaveLocalB:
-        title_trie.save_local()
+    # 保存前缀树到文件中
+    if SaveLocal:
+        save_file = "./test/tile_tire_t.json"
+        title_trie.save_local(save_file)
 
     # 测试检索整个字符串,返回符合的匹配项
     if Test_search_all:
-        Test_search_all_fromFile = 0
-        Test_search_all_fromStr = 0
-        Test_search_all_fromStrLines = 1
+        Test_search_all_fromFile = 0  # 从文件中查找
+        Test_search_all_fromStr = 0  # 从字符串中查找
+        Test_search_all_fromStrLines = 1  # 从多行字符串中查找
         # 多行字符串转list
         if Test_search_all_fromStrLines:
             l_t = str_t.split("\n")
-            list_t = []
-            for line in l_t:
-                list_t.append(eval(line))
-            # search_all
-            for line in list_t:
+            for idx, line in enumerate(l_t, start=1):
+                line = eval(line)
                 result = title_trie.search_all(line[0])
                 if result:
-                    logger.info(f"get {result} from {line[0]}")
+                    logger.info(f"get {result} from {line[0]} line: {idx}")
         elif Test_search_all_fromStr:
             str_t2 = "哈尔滨音乐学院led食堂大宗食品面板采购项目（二次）-招标公告"
             logger.info(
