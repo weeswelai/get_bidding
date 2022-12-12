c677f337ba8ceb163791d3a44676d514f4c3ff14
weeswelai <52352067+weeswelai@users.noreply.github.com>
2022-12-12 15:40:35 +0800

    refactor: use bid and bid_tag obj

--- module/bid_task.py
+++ module/bid_task.py
@@ -8,23 +8,25 @@
 """
 from datetime import datetime
 from time import sleep
+from bs4 import Tag
 import traceback
 import sys
 
 from module.bid_log import logger
-from module.bid_web_brows import web_brows, bid_web
+from module.bid_web_brows import web_brows, bid_web, bid_tag, bid
 from module.utils import *
 from module.bid_judge_content import title_trie
 
 
 class TaskManager:
     task_name: str
-    list_open_url = []
-    list_url: str  # 当前正则浏览的的项目列表网址
+    list_open_url: list = None
+    list_url: list  # 当前正则浏览的的项目列表网址
     end_rule: dict  # 翻页结束标志
     state: str
     last_newest: bool  # last.newest是否有值
-    match_list = []
+    bid_list: list or tuple = None
+    match_list: list = None
 
     def __init__(self, json_file, save=True, creat_new=False):
         """ 读取json_file; 设置 settings, json_file , queue
@@ -36,9 +38,11 @@ class TaskManager:
         logger.hr("bid_task.init", 3)
         self.settings = read_json(json_file)  # json文件内容
         self.json_file = json_file  # json文件路径
-        self.list_file = deep_get(self.settings, "task.save_data")
-        self.match_list_file = deep_get(self.settings, "task.match_data")
-        creat_folder(self.list_file)
+        list_file = deep_get(self.settings, "task.save_data")
+        match_file = deep_get(self.settings, "task.match_data")
+        creat_folder(list_file)
+        self.list_file = open(list_file, "a", encoding="utf-8")
+        self.match_list_file = open(match_file, "a", encoding="utf-8")
 
         logger.info(f"task queue = {deep_get(self.settings, 'task.queue')}")
 
@@ -55,20 +59,32 @@ class TaskManager:
         logger.hr("bid_task.build_new_task", 3)
         # 若new_name有值且在queue中,将new_name排到最前面
         self._creat_new_queue(new_name)
-        self.task_name = self.queue[0]  # 取得 task name
-        self.end_rule, self.state = self._get_end_rule()  # 判断任务状态
+        self._get_end_rule()  # 判断任务状态
         deep_set(self.settings, "task.name", self.task_name)
         deep_set(self.settings, "task.run_time", date_now_s())  # 写入运行时间
+        self.init_brows()
 
-        web_brows.init(deep_get(self.settings, f"{self.task_name}"))
-        # bid_web.init()
-
-        logger.info(f"url_root: {web_brows.url_root}")
+        logger.info(f"url_root: {bid.url_root}")
         deep_set(self.settings, "task.end_rule", self.end_rule)  # 保存 end_rule
         deep_set(self.settings, "task.last_complete", self.state)  # 保存 state
         logger.info(f"build new task {self.task_name} complete")
         save_json(self.settings, self.json_file)
         return self.task_name
+
+    def _creat_new_queue(self, new_name):
+        if not new_name and new_name in self.queue:
+            idx = self.queue.index(new_name)
+            for i in range(idx):
+                self.queue.append(self.queue.pop(0))
+            logger.info(f"new task start at {new_name}")
+        else:
+            self.task_name = self.queue[0]
+
+    def init_brows(self):
+        settings = self.settings[f"{self.task_name}"]
+        web_brows.init(settings)
+        bid_tag.init(settings)
+        bid.init(settings)
 
     def build_list_pages_brows(self, list_url_idx=0):
         """
@@ -81,126 +97,63 @@ class TaskManager:
         if not self.list_open_url:  # 若list_brows_url无实际值, 从json中获得url
             self.list_open_url = deep_get(
                 self.settings,
-                f"{self.task_name}.url_open.list_url")
+                f"{self.task_name}.url.list")
             self.list_url = self.list_open_url[list_url_idx]
         else:
             self.list_url = web_brows.get_next_pages(self.list_url)
         deep_set(self.settings, f"{self.task_name}.last.taskBreakUrl",
                  self.list_url)
-        deep_set(self.settings, "task.web_list.list_url", self.list_url)
+        deep_set(self.settings, "task.web_list.url", self.list_url)
         logger.info(f"list_brows_url: {self.list_open_url}\n" +
                     f"list_url: {self.list_url}")
         save_json(self.settings, self.json_file)
 
-    def get_list_from_list_web_html(self, reOpen=0):
+    def get_list_from_list_web_html(self):
         """
         调用web_brows对象对列表网页的源码进行解析，获得项目列表
         若cut_html出错,可能是网络波动,导致网站服务器返回的数据缺失,将重新打开页面
         """
         logger.hr("bid_task.get_list_from_list_web_html", 3)
-        # TODO 计算时间 定时 打开网页 打开网页出错后异常处理 解析网页 解析网页后异常处理 获得列表
+        # TODO 计算时间 定时 解析网页 解析网页后异常处理 获得列表
         # TODO 列表解析 保存列表
         # 获得list, 包括 url date name b_type
         try:
-            web_brows.cut_html()
+            tag_list = web_brows.get_bs_tag_list()
         except Exception:
-            # TODO 识别出错的网页
-            logger.error(f"{traceback.format_exc()}")
-            web_brows.save_response(save_date=True, extra="list_Error")
-            logger.info(f"cut html error,open {self.list_url} again" + \
-                        f"\nreOpen: {reOpen}")
-            if reOpen < 3:
-                reOpen += 1
-                sleep(1.5)
-                self.open_list_url()
-                self.get_list_from_list_web_html(reOpen)
-            else:
-                logger.error(f"{self.list_url} open more than {reOpen} time")
-        try:
-            web_brows.get_list()
-        except Exception as e:
-            output, temp = str_list(web_brows.bid_list)
-            logger.info(f"error bid list now is {output}")
-            logger.error(f"error at li_tag[{web_brows.list_idx}]\n" +
-                         f"error tag: \"{web_brows.bs_tag}\"")
-            logger.error(f"{traceback.format_exc()}")
-
+            logger.error(f"\n{traceback.format_exc()}")
             web_brows.save_response(save_date=True, extra="list_Error")
             web_brows.save_response(rps=web_brows.html_list_match,
                                     save_date=True, extra="cut_list_Error")
+        self.bid_list = ()
+        try:
+            for idx, tag in enumerate(tag_list):
+                bid.receive(*bid_tag.get(tag))
+                if self.state == "break":
+                    if not _bid_is_start(bid):
+                        logger.info(f"start at {bid.message}")
+                        self.process_bid(bid)
+                    continue
+                elif self.state in (None, "", "complete"):
+                    self.process_bid(bid)
+                if _bid_is_end(bid, self.end_rule):
+                    self._complete_task(bid.message)
+        except Exception:
+            output = str_list(self.bid_list)[0]
+            logger.error(f"{traceback.format_exc()}")
+            logger.info(f"error bid list now is \n{output}")
+            logger.error(f"error at li_tag[{idx}]\n" +
+                         f"error tag: \"{str(tag)}\"")
+        self._state()
 
-        if self.state in ("", " ", None) and not self.last_newest:  # 任务上次状态未记录
-            newest = _bid_list_element_to_dict(web_brows.bid_list, 0)
-            newest["date"] = deep_get(self.settings,
-                                      "task.run_time")  # 日期改为当前运行时间
-            deep_set(self.settings, f"{self.task_name}.last.newest", newest)
-            logger.info(f"{self.task_name}.last.newest: {newest}")
+    def process_bid(self, bid_prj):
+        self.list_file.write(f"{str(bid_prj.message)}\n")
+        result = title_trie.search_all(bid_prj.name)
+        if result:
+            result.append(bid_prj.message)
+            self.match_list_file.write(f"{str(result)}\n")
+        self.bid_list += (bid_prj.message,)
 
-    def process_bid_list(self):
-        """
-        """
-        logger.hr("bid_task.process_bid_list", 3)
-
-        # TODO 遍历列表中的标题，对于标题，招标类型符合条件的(调用判断模块),这里符合条件的是
-        #  标题、不为已记录过的，日期大于停止条件的、类型为货物的(暂不判断类型) 遍历列表,找到符合项
-        self.match_list = []
-        idx_list = []
-        task_next = True  # complete, break
-        with open(self.list_file, "a", encoding="utf-8") as f_bid_list, \
-                open(self.match_list_file, "a",
-                     encoding="utf-8") as f_match_list:
-            # TODO 区分 break 和 complete 和 None的状态
-            if self.state in ("", None, "complete"):
-                for idx, bid in enumerate(web_brows.bid_list):
-                    if _bid_is_end(bid, self.end_rule):
-                        task_next = False
-                        self._complete_task(idx)
-                    else:
-                        result = title_trie.search_all(bid[0])
-                        f_bid_list.write(f"{''.join(bid)}\n")  # TODO 写入txt形式
-                        if result:
-                            idx_list.append(idx)
-                            result += bid
-                            self.match_list.append(result)
-                            f_match_list.write(f"{','.join(result)}\n")
-                    if not task_next:
-                        break
-            elif self.state == "complete":
-                # 判断
-                pass
-            elif self.state == "break":
-                pass
-        # TODO 将 match_list 存进文件中
-
-        logger.info(f"match index:{idx_list}")
-        logger.info(f"match_list:\n{str_list(self.match_list)[0].strip()}")
-
-        if self.state == "break":
-            task_break = _bid_list_element_to_dict(web_brows.bid_list, -1)
-            deep_set(self.settings,
-                     f"{self.task_name}.last.task_break", task_break)
-            logger.info(f"{self.task_name}.last.task_break: {task_break}")
-
-        return task_next
-
-    def save_match_list(self):
-        """
-        TODO 保存经过前缀树检索后得到的list,保存 bid_list中的信息和match_list的匹配结果
-        """
-        pass
-
-    # TODO 由self.match_list中的索引找到原 bid_list 拼接招标项目的网址,之后就可以用BidHtml 分析页面
-    # def 
-
-    def timer(self):
-        """
-
-        Returns:
-
-        """
-        pass
-
-    def open_list_url(self):
+    def open_list_url(self, reOpen=0):
         """ 打开浏览页面，获得页面招标信息列表
         """
         logger.hr("bid_task.open_list_url", 3)
@@ -209,55 +162,83 @@ class TaskManager:
         web_brows.init_req(url=self.list_url)
         logger.info(f"open {self.list_url}")
         web_brows.open_url_get_response()
+        try:  # 在打开网页后立刻判断网页源码是否符合要求
+            web_brows.cut_html()  # 必须执行依次cut_html看网站有没有正常获得
+        except Exception:
+            # TODO 识别出错的网页
+            logger.error(f"{traceback.format_exc()}")
+            web_brows.save_response(save_date=True, extra="list_Error")
+            logger.info(f"cut html error,open {self.list_url} again" + \
+                        f"\nreOpen: {reOpen}")
+            if reOpen < 3:
+                reOpen += 1
+                sleep(1.5)  # TODO 换定时器
+                self.open_list_url()
+            else:
+                logger.error(f"{self.list_url} open more than {reOpen} time")
+                # TODO 这里需要一个保存额外错误日志以记录当前出错的网址
 
     def _get_end_rule(self):
         """返回任务的结束规则,根据 self.taskname.last.complete的值返回end_rule
         Return:
-            end_rule (str or list): 停止条件:
+            end_rule (dict): 停止条件:
                 1. str: 可能为招标项目名或时间
                 2. list: 招标项目名和时间的组合
             state (str or None): 上次任务状态
         """
-        end_rule = {}
+        self.end_rule = {key: "" for key in ["name", "date", "url"]}
         last_task = deep_get(self.settings, f"{self.task_name}.last")
-        state = deep_get(last_task, "complete")  # 判断该网站上次任务状态
-        if state in ("", " ", None):  # 既不是break 也不是 complete
+        self.state = deep_get(last_task, "complete")  # 判断该网站上次任务状态
+        if self.state in ("", " ", None):  # 既不是break 也不是 complete
             # 以6天为期限算截止条件
             if not last_task["end_rule"]["date"]:
-                end_rule["date"] = date_days(change_days=-6)
+                self.end_rule["date"] = date_days(change_days=-6)
             else:
-                end_rule = last_task["end_rule"]
-        elif state == "break":
-            end_rule = deep_get(last_task, "last.end_rule")
+                self.end_rule = last_task["end_rule"]
+        elif self.state == "break":
+            self.end_rule = deep_get(last_task, "last.end_rule")
         # 若上次任务已完成,则判断newest
-        elif state == "complete":
-            web_list = deep_get(last_task, "web_list")
-            end_rule = [web_list["first_name"], web_list["first_date"]]
+        elif self.state == "complete":
+            self.end_rule = deep_get(last_task, "newest")
         else:
-            logger.error(f"error complete flag: {state}")
+            logger.error(f"error complete flag: {self.state}")
             sys.exit(1)
-
+        if len(self.end_rule["date"]) <= 10:
+            self.end_rule["date"] = self.end_rule["date"] + " 00:00:00"
         # 上次任务未记录
         self.last_newest = False if last_task["newest"]["name"] == "" else True
-        logger.info(f"{self.task_name}.last.complete is: {state}")
-        logger.info(f"end_rule : {end_rule}")
-        return end_rule, state
+        logger.info(f"{self.task_name}.last.complete is: {self.state}")
+        logger.info(f"end_rule : {self.end_rule}")
 
-    def _creat_new_queue(self, new_name):
-        if not new_name and new_name in self.queue:
-            idx = self.queue.index(new_name)
-            for i in range(idx):
-                self.queue.append(self.queue.pop(0))
-            logger.info(f"new task start at {new_name}")
-
-    def _complete_task(self, bid):
+    def _complete_task(self, bid_prj):
         if self.state in ("", " ", None):
-            bid = deep_get(self.settings, f"{self.task_name}.last.newest")
+            bid_prj = deep_get(self.settings, f"{self.task_name}.last.newest")
         self.state = "complete"
         deep_set(self.settings, f"{self.task_name}.last.complete", "complete")
-        deep_set(self.settings, f"{self.task_name}.last.end_rule", bid)
+        deep_set(
+            self.settings,
+            f"{self.task_name}.last.end_rule",
+            _bid_to_dict(bid_prj))
         save_json(self.settings, self.json_file)
+        sys.exit()
 
+    def _state(self):
+        if self.state in ("", " ", None) and not self.last_newest:  # 任务上次状态未记录
+            newest = _bid_to_dict(bid.message)
+            newest["date"] = deep_get(self.settings,
+                                      "task.run_time")  # 日期改为当前运行时间
+            deep_set(self.settings, f"{self.task_name}.last.newest", newest)
+            logger.info(f"{self.task_name}.last.newest: {newest}")
+        
+        # if self.state == "break":
+        #     task_break = _bid_list_element_to_dict(web_brows.bid_list, -1)
+        #     deep_set(self.settings,
+        #              f"{self.task_name}.last.task_break", task_break)
+        #     logger.info(f"{self.task_name}.last.task_break: {task_break}")
+
+    def close(self):
+        self.list_file.close()
+        self.match_list_file.close()
 
 def _date_is_end(date: str, end_date: str, date_len):
     if date_len > 10:
@@ -271,25 +252,41 @@ def _date_is_end(date: str, end_date: str, date_len):
 def _bid_is_end(bid, end_rule):
     # TODO 判断日期大小
     is_end = False
-    if bid[0] == end_rule["name"] or bid[3] == end_rule["url"]:
+    if bid.name == end_rule["name"] or bid.url == end_rule["url"]:
         is_end = True
     if end_rule["date"]:
-        if _date_is_end(bid[1], end_rule["date"], len(bid[1])):
+        if _date_is_end(bid.date, end_rule["date"], len(bid.date)):
             is_end = True
     logger.debug(f"_bid_is_end is {is_end}")
     return is_end
 
 
-def _bid_list_element_to_dict(bid_list, idx):
+def _bid_is_start(bidPrj, task_break):
+    for key in task_break:
+        if getattr(bidPrj, key) == task_break[key]:
+            return True
+    return False
+
+
+def _bid_list_element_to_dict(bid_list, idx=0):
     return _bid_to_dict(bid_list[idx])
 
 
-def _bid_to_dict(bid):
-    return {
-        "name": bid[0],
-        "date": bid[1],
-        "url": bid[2]
-    }
+def _bid_to_dict(bid_prj: list):
+    if isinstance(bid_prj, list):
+        return {
+            "name": bid_prj[0],
+            "date": bid_prj[1],
+            "url": bid_prj[2]
+        }
+    elif isinstance(bid_prj, dict):
+        return bid_prj
+    else:
+        return {
+            "name": "",
+            "date": "",
+            "url": ""
+        }
 
 
 if __name__ == "__main__":
--- module/bid_web_brows.py
+++ module/bid_web_brows.py
@@ -7,6 +7,10 @@
  2. 遍历列表中的标题，对于标题，招标类型符合条件的(调用判断模块)
     2.1 交给任务调度模块判断是否已经获取过，若已获取过则停止并返回任务调度进行下一任务
     若没获取过则继续
+    在 bid_web_brows 里调用bid_task 
+    或 在 bid_task 里得到bid_web_brows 的返回值
+    或 在 bid_task 里使用全局变量(全局对象)
+    同时 对符合条件的
     (这里不确定是先交给任务调度判断是否已获取过还是先判断是否符合条件)
     2.2 记录符合条件的项目 的 url 、 标题 、 时间 储存到文件中(可能会用到数据库)
     2.3 打开符合条件的项目的页面 、 调用页面模块对项目页面进行读取，由于涉及到访问频率，
@@ -15,10 +19,12 @@
  4. 由任务调度来循环 1~3
 """
 import re
+import traceback
 from bs4 import BeautifulSoup as btfs
+from bs4 import Tag
 from sys import getsizeof
 
-from module.get_url import WebFromUrlOpen
+from module.get_url import UrlOpen
 from module.utils import *
 from module.bid_log import logger
 from module.bid_judge_content import title_trie
@@ -28,41 +34,162 @@ HEADER = {
 }
 
 
-def get_bid_url(url_root, bid_root, bid_tail):
-    """ 用 前缀加上后缀得到网址
-    输入bid_root对 json中的 name.url_open.url_root 进行查表
-    Args:
-        bid_root (str): 前缀索引
-        bid_tail (str): 后缀
-    """
-    if bid_root in url_root:
-        return f"{url_root[bid_root]}{bid_tail}"
-    else:
-        return f"{url_root['root']}{bid_tail}"
+# 仅对WebBrows中的li_tag进行操作
+class BidTag:
+    type_r: tuple 
+    url_r: tuple 
+    date_r: tuple 
+    name_r: tuple 
+    tag: Tag
+
+    def init(self, settings):
+        rule = settings["rule"]["bid_tag"]
+        for li_r in rule:
+            self._init_list_rule(li_r, rule[li_r])
+
+    def _init_list_rule(self, li_r, rule):
+        """
+        """
+        # 预处理分割rule
+        find_all_idx = value_name = value = None
+        if rule.count("|") == 2:  # 2个|认为有 find_all_idx, 用于find_all的索引
+            tag_gets_r, value_gets_r, find_all_idx = rule.split("|")
+        elif rule.count("|") == 1:  # 必须有1个 |
+            tag_gets_r, value_gets_r = rule.split("|")
+        if tag_gets_r:
+            tag_find, tag_name = tag_gets_r.split(":")
+        if value_gets_r:
+            value_name, value = value_gets_r.split(":")
+        setattr(
+            self, li_r, (tag_find, tag_name, find_all_idx, value_name, value))
+
+    def get(self, bid_tag):
+        """
+        """
+        b_type = self.parse_bs_rule(bid_tag, *self.type_r)  # 项目类型 货物 工程 服务
+        url = self.parse_bs_rule(bid_tag, *self.url_r)
+        date = self.parse_bs_rule(bid_tag, *self.date_r) # 正则过滤,取出数字部分
+        name = self.parse_bs_rule(bid_tag, *self.name_r)  # 标题
+        return [name, date, url, b_type]
+
+    def parse_bs_rule(self, tag: Tag,
+                      tag_find="", tag_name="", find_all_idx=None,
+                      value_name="", value="",) -> Tag or None or str:
+        """
+        解析规则,找到tag或tagList(bs4.element.ResultSet),或符合规则的属性值或tag中的文本
+        Args:
+            tag (bs4.element.Tag): 要检索的tag
+        Returns:
+            tag or text or None, 可能有三种返回
+            None: not rule 为 True时返回None
+            tag (bs4.element.Tag, bs4.element.ResultSet): find或find_all的检索结果
+            text (str): tag的内容文本, 或tag中符合规则的属性的值
+        """
+        if not tag_find:  # 无rule返回None
+            return None
+        
+        # 检索tag
+        if tag_find == "tagName_all":
+            if value_name and value:  # 若有value检索要求,则find_all加上参数attrs
+                tag = tag.find_all(tag_name, attrs={value_name: value})
+            # 无value要求,直接用find_all搜索
+            # 使用find_all(tag_name, attrs={"":""}) 会返回None,所以这里额外调用语句
+            else:
+                tag = tag.find_all(tag_name)
+            if find_all_idx:  # 若有find_all 的索引要求,则返回该索引对应的tag
+                tag = tag[int(find_all_idx)]
+        elif tag_find == "tagName_find":  # 使用tagName,find方式检索
+            tag = bs_deep_get(tag, tag_name)  # 调用额外函数返回Tag
+        self.tag = tag
+        # 检索属性值
+        if value_name:  # value_name有值 则检索属性值
+            if value_name == "class":
+                if "".join(tag.get("class")) == value.replace(" ", ""):  # class=value 的 text值
+                    return tag.text.strip()  # tag内容文本
+                else:  # 若当前tag的class不符合则用find
+                    return tag.find(class_=value).text.strip()  # tag内容文本
+            elif value_name == "_Text":  # 没有属性只有text的标签
+                return tag.text.strip()  # tag内容文本
+            else:
+                return tag.get(value_name)  # tag属性值
+        else:  # 若不检索属性值则直接返回tag或
+            return tag   
 
 
-class WebBrows(WebFromUrlOpen):
+class Bid:
+    b_type: str
+    url: str
+    date: str
+    name: str
+    date_cut: re.Pattern
+    url_root: dict
+    message: list = None
+
+    def init(self, settings):
+        rule = settings["rule"]["bid"]
+        for r in rule:
+            setattr(self, r, init_re(rule[r]))
+        self.url_root = deep_get(settings, "url.root")
+
+    def receive(self, name, date, url, b_type):
+        self.name = name
+        self.date = self.re_get_date_str(date)
+        self.get_bid_url(b_type, url)
+        self.b_type = b_type
+        bid.message = [self.name, self.date, self.url, self.b_type]
+
+    def re_get_date_str(self, date_str, date_cut_rule=None):
+        """
+        处理带时间的字符串时间
+        Args:
+            date_str (str): 要裁剪的时间变量, 通常为 "发布时间: 2022-11-11"
+            date_cut_rule (str): 裁剪规则,仅在测试时使用
+        Returns:
+
+        """
+        # 默认正则参数 (4)(数字)+(_,-,年)+(2)(数字)+(_,-,月)+(2)(数字)+(日),日可忽略
+        if isinstance(date_cut_rule, str):
+            return re.search(date_cut_rule, date_str).group()
+        if self.date_cut.pattern == "":
+            return date_str
+        return self.date_cut.search(date_str).group()
+    
+    def get_bid_url(self, bid_root, bid_tail):
+        """ 用 前缀加上后缀得到网址
+        输入bid_root对 json中的 name.url_open.url_root 进行查表
+        Args:
+            bid_root (str): 前缀索引
+            bid_tail (str): 后缀
+        """
+        if bid_root in self.url_root:
+            self.url = f"{self.url_root[bid_root]}{bid_tail}"
+        else:
+            self.url = f"{self.url_root['default']}{bid_tail}"
+
+    def return_bid(self):
+        return self.message
+
+    def is_end(self, end_rule):
+        pass
+
+
+class WebBrows(UrlOpen):
     cut_rule: re.Pattern = None  # init_re
     next_pages_rule: re.Pattern = None  # init_re
-    list_rule = {}
-    date_cut_rule: re.Pattern = None  # init_re
     html_list_match = ""
-    # next_pages_rule = ""
-    list_idx = 0
-    bid_list = []
-    url_root = {}
     cutHtml: bool
+    bs: Tag = None
 
     def init(self, settings):
         # 若有cookie 需求(使用第一个cookie)
-        cookie = deep_get(settings, "url_open_cookie")
+        cookie = deep_get(settings, "url.cookie")
         if cookie:
             self.update_req(cookie=cookie[0])
-        self.url_root = deep_get(settings, "url_open.url_root")
-        # 项目列表页面爬取规则
-        self.init_rule(deep_get(settings, "brows_rule"))
+        # init rule
+        self.cut_rule = init_re(deep_get(settings, "rule.cut"))
+        self.next_pages_rule = init_re(deep_get(settings, "rule.next_pages"))
 
-    def get_list(self, page=None, list_rule: dict = None, parse="html.parser"):
+    def get_bs_tag_list(self, page=None, parse="html.parser"):
         """
         输入 str 调用 bs生成self.bs 从self.bs 里根据list_rule提取list
         Args:
@@ -72,38 +199,16 @@ class WebBrows(WebFromUrlOpen):
         Returns:
             bid_list (list): 提取到的list
         """
-        self.bid_list = []  # 重置
-        logger.hr("bid_web_brows.get_list", 3)
+        logger.hr("bid_web_brows.get_bs_tag_list", 3)
         if isinstance(page, str):
             self.bs = btfs(page, features=parse)  # 获得bs解析结果
         else:
-            self.bs = btfs(self.html_list_match, features=parse)  # 获得bs解析结果
+            self.bs = btfs(self.html_list_match, features=parse)  # bs解析结果
 
-        logger.info(
-            "get list from " +
-            f"""{page if page in ['url_response', 'html_list_match'] else
-            'html read'}""")
-        if not list_rule:
-            list_rule = self.list_rule
-
-        li_r, name_r, date_r, url_r, type_r = list_rule.values()
-
-        tag_list = self.parse_bs_rule(self.bs, li_r)
-        for idx, li_tag in enumerate(tag_list):
-            self.list_idx = idx
-            b_type = self.parse_bs_rule(li_tag, type_r)  # 项目类型 货物 工程 服务
-            url = get_bid_url(
-                self.url_root, b_type, self.parse_bs_rule(li_tag, url_r))  # url
-            date = self.re_get_date_str(
-                self.parse_bs_rule(li_tag, date_r)
-            )  # 正则过滤,取出数字部分
-            name = self.parse_bs_rule(li_tag, name_r)  # 标题
-            bid_prj = [name, date, url, b_type]  # 合并为list
-            self.bid_list.append(bid_prj)  # 加入当前总表
-            # logger.info(f"list {idx} complete")  # 调试日志
-        logger.info(f"get bid list :\n{str_list(self.bid_list)[0]}")
-        self.list_idx = self.bs_tag = None
-        return self.bid_list
+        logger.info("get tag list from " +
+                    f"{page[: 100] if page  else 'html read'} parse={parse}")
+        
+        return bid_tag.parse_bs_rule(self.bs, *bid_tag.li_r)
 
     def cut_html(self, _cut_rule: dict = None):
         """
@@ -124,28 +229,6 @@ class WebBrows(WebFromUrlOpen):
             self.html_list_match = \
                 self.cut_rule.search(self.url_response).group()
         self.cutHtml = True
-        # response_s = getsizeof(self.url_response)
-        # match_s = getsizeof(self.html_list_match)
-        # logger.info(f"size of response: {response_s},size of response_match: " +
-        #             f"{match_s} Difference: {response_s-match_s}")
-
-        return self.html_list_match
-
-    def re_get_date_str(self, date_str, date_cut_rule=None):
-        """
-        处理带时间的字符串时间
-        Args:
-            date_str (str): 要裁剪的时间变量, 通常为 "发布时间: 2022-11-11"
-            date_cut_rule (str): 裁剪规则,仅在测试时使用
-        Returns:
-
-        """
-        # 默认正则参数 (4)(数字)+(_,-,年)+(2)(数字)+(_,-,月)+(2)(数字)+(日),日可忽略
-        if isinstance(date_cut_rule, str):
-            return re.search(date_cut_rule, date_str).group()
-        if self.date_cut_rule.pattern == "":
-            return date_str
-        return self.date_cut_rule.search(date_str).group()
 
     def init_rule(self, rule):
         """
@@ -156,15 +239,9 @@ class WebBrows(WebFromUrlOpen):
         Returns:
             bool: rule有值返回 True,无值返回 False
         """
-        logger.info("bid_web_brows.init_rule")
-        if rule:
-            for r in rule:
-                if r == "re":
-                    for re_rule in rule["re"]:
-                        setattr(self, re_rule, init_re(rule["re"][re_rule]))
-                else:
-                    setattr(self, r, rule[r])
-        logger.info(f"rule: {dumps(rule, indent=2, ensure_ascii=False)}")
+        pass
+        # logger.info("bid_web_brows.init_rule")
+        # logger.info(f"rule: {dumps(rule, indent=2, ensure_ascii=False)}")
 
     def get_next_pages(self, page_url, next_pages_rule=None):
         """
@@ -180,9 +257,12 @@ class WebBrows(WebFromUrlOpen):
         return next_pages_url
 
 
-class BidHtml(WebFromUrlOpen):
+class BidHtml(UrlOpen):
     pass
 
 
+bid_tag = BidTag()
+bid = Bid()
 web_brows = WebBrows(headers=HEADER)
 bid_web = BidHtml(headers=HEADER)
+
