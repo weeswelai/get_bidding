f9d7ef7e64408717e307d4ab8cf511e11cf36ad4
weeswelai <52352067+weeswelai@users.noreply.github.com>
2022-12-18 01:49:31 +0800

    refactor: 重写逻辑,不影响原功能 (#11)
    删除部分变量,修改BidTask变量名, 修改注释

--- module/bid_task.py
+++ module/bid_task.py
@@ -19,17 +19,17 @@ from module.bid_log import logger
 from module.bid_web_brows import *
 from module.utils import *
 
+data_path = r"./data"
 
 
 class State:
     settings: dict  # 当前task 的settings 如 zzlh.task1
     newest = False
     start = True  # process_tag_list 中判断
+    state = ""  # 默认为 ""
 
     def __init__(self, settings, state_idx="test") -> None:
         self.state = settings["complete"]
-        if self.state == "complete":
-            self.state = ""
         self.end_rule = settings["end_rule"]  # 翻页结束标志
         self.list_url = settings["url"]
         self.state_idx = state_idx
@@ -47,7 +47,9 @@ class State:
                     f"end_rule : {self.end_rule}")
 
     def bid_is_end(self, bid_prj: Bid):
-        """
+        """ 判断当前项目是否符合结束条件
+        Args:
+            bid_prj (<class> Bid): 当前Bid对象, 保存项目信息
         """
         if bid_prj.name == self.end_rule["name"] \
                 and bid_prj.url == self.end_rule["url"]:
@@ -64,7 +66,9 @@ class State:
 
     def complete(self):
         """ 完成任务后, 将newest 设为 end_rule, 清除 newest 和 interrupt设置
+        将 BidTask.State.state 设为 "complete"
         """
+        logger.info("bid is end")
         self.state = "complete"
         if deep_get(self.settings, "newest.name") != "":
             deep_set(self.settings, "end_rule", self.settings["newest"])
@@ -75,7 +79,7 @@ class State:
 
     def save_newest_and_interrupt(self, bid: Bid):
         """ 保存最新的招标项目信息, 设置 compelete 为 interrupt
-            仅执行一次
+            仅执行一次, interrupt状态下不执行
         """
         bid_message = _bid_to_dict(bid)
         if len(bid_message["date"]) < 10:
@@ -124,18 +128,24 @@ class InterruptState(State):
             exit()
 
     def bid_is_start(self, bid_prj: Bid):
-        """ 三个信息必须全部符合
+        """判断条件为: name, date, url 三个信息必须全部符合, 符合返回True 并
+        将 InterruptState.state 置为 True, 不符合 返回 False .
+        仅在 interrupt状态下判断输入项目是否符合条件,
+        json中state.interrupt 信息为自动生成, 若手动填写请注意是否填写正确,否则
+        可能导致一直到符合 end_rule 并结束state 时都没有符合开始条件
+        
         Args:
             bid_prj: self.bid
         """
-        for key in self.interrupt:
-            if getattr(bid_prj, key) == self.interrupt[key] and \
-                    self.interrupt[key] != "" :
+        for key in self.interrupt:  # name, date, url
+            if getattr(bid_prj, key) == self.interrupt[key] \
+                and self.interrupt[key] != "" :
                 continue
-            else:
-                return None  # 不满足则退出判断
+            else:  # 有一个不符合条件直接返回False
+                return False  # 不满足则退出判断
         logger.info("bid is start")
         self.start = True
+        return True
 
     def save_newest(self, *args):
         # interrupt状态不执行
@@ -153,12 +163,11 @@ class InterruptState(State):
                          "please check settings json file")
             exit()
 
-
+# TODO 初始化和 State 初始化, 以及 state重新初始化
 class BidTask:
-    settings: dict
-    task: dict  # init at _get_next_task
-    state_idx: str  # init at _get_next_task
-    state: State or InterruptState  # init at _get_state_from_task
+    settings: dict  # 初始化新的State时用到
+    state_idx: str  # init at _get_state  "state1" or "state2"
+    State: InterruptState  # 也有可能是State类 init at _get_state_from_task
     bid: Bid
     bid_tag: BidTag
     web_brows: WebBrows
@@ -173,94 +182,104 @@ class BidTask:
         self.settings = settings  # zzlh:{}
         self.task_name = task_name  # 当前任务名
         logger.info(f"init task {self.task_name}")
-        self.init_brows(settings)  # input()
+        self._init_brows(settings)
         self._creat_save_file()
-        # 取一个任务, 若queue 为空报错退出
-        # self.init_state(settings)  # 由 TaskManager执行
+
+    def _init_brows(self, settings):
+        """ 初始化网页对象模型, 只在初始化时调用一次
         
-    def init_state(self, settings=None):
-        logger.info("bid_task.Bid.init_state")
-        if settings is None:
-            settings = self.settings
-        if self._get_state_idx(deep_get(settings, "task.queue")):
-            self._get_state(self.task, self.state_idx)  # 获得当前state
-            return True
-        return False
-
-    def _creat_save_file(self):
-        """ 创建数据保存文件, add 方式
-        """
-        list_save = f"./data/bid_list_{self.task_name}.txt"
-        match_list_save = f"./data/bid_match_list_{self.task_name}.txt"
-        creat_folder(list_save)
-        self.list_file = open(list_save, "a", encoding="utf-8")
-        self.match_list_file = open(match_list_save, "a", encoding="utf-8")
-
-    def _get_state_idx(self, queue: list):
-        """ 从queue中取下一个任务赋给 self.task_name(str) 和 self.task(dict),
-            若 queue 为空返回False
-        """
-        if queue:
-            self.state_idx = queue[0]
-            self.task = deep_get(self.settings, f"{self.state_idx}")
-            return True
-        else:
-            logger.info(f"{self.task_name}.queue is []")
-            return False
-
-    def _get_state(self, task: dict, state_idx: str):
-        """ 判断当前task, 调用相应的state对象
         Args:
-            task (dict): json 中task_name.state 的value
-            state_idx (str): json中 task_name.state 的key 
-        """
-        self.state = InterruptState(task, state_idx) \
-            if task["complete"] == "interrupt" else State(task, state_idx)
-        logger.info(f"BidTask.state type = \"{task['complete']}\"")
-        self.state.print_state()
-
-    def init_brows(self, settings):
-        """ 初始化网页对象模型
+            settings (dict): json中 的具体任务
         """
         self.bid_tag = BidTag(settings)
         self.bid = Bid(settings)
         self.web_brows = WebBrows(settings)
         self.bid_web = BidHtml(settings)
-        logger.info(f"url settings: {jsdump(settings['url'])}")
-        logger.info(f"rule: {jsdump(settings['rule'])}")
+        logger.info(f"url settings:\n{jsdump(settings['url'])}\n"
+                    f"rule:\n{jsdump(settings['rule'])}")
+
+    def _creat_save_file(self):
+        """ 创建数据保存文件, add 方式
+        """
+        list_save = f"{data_path}/bid_list_{self.task_name}.txt"
+        match_list_save = f"{data_path}/bid_match_list_{self.task_name}.txt"
+        creat_folder(list_save)
+        self.list_file = open(list_save, "a", encoding="utf-8")
+        self.match_list_file = open(match_list_save, "a", encoding="utf-8")
+        # 写入一行运行时间
+        self.list_file.write(date_now_s())
+        self.match_list_file.write(date_now_s())
+
+    def _get_state_idx(self, queue: list):
+        """ 从queue中取第一个state 赋给 self.state_idx(str)
+        若 queue 为空返回False
+        """
+        if queue:
+            self.state_idx = queue[0]
+            logger.info(f"BidTask._get_state_idx = {self.state_idx}")
+            return True
+        else:
+            logger.info(f"{self.task_name}.queue is []")
+            return False
+
+    def _init_State(self, state_idx: str):
+        """ 判断state_idx中的 complete, 初始化相应的State对象
+
+        Args:
+            state_idx (str): json中 task_name.state 的key, 例如 state1
+        """
+        setting = self.settings[state_idx]
+        self.State = InterruptState(setting, state_idx) \
+            if setting["complete"] == "interrupt" else State(setting, state_idx)
+        logger.info(f"json: {self.task_name}{self.state_idx}.state= "
+                    f"\"{setting['complete']}\"")
+        self.State.print_state()
+
+    def init_state(self, settings=None):
+        """ 若 task.stateQueue 中还有state, 初始化State
+        
+        Returns:
+            (bool): 初始化完成返回 True ,失败返回 False 
+        """
+        logger.info("BidTask.init_state")
+        # 若queue中还有state
+        if self._get_state_idx(deep_get(self.settings, "stateQueue")):
+            self._init_State(self.state_idx)
+            return True
+        return False
 
     def _get_ist_url(self):
         """ 获得下次打开的 url 保存在self.list_url
         """
         if not self.list_url:
-            self.list_url = self.state.return_url()
+            self.list_url = self.State.return_url()
         else:
             self.list_url = self.web_brows.get_next_pages(self.list_url)
 
-    # def restart(self):
-    #     """ 将json中 complete 添加到 queue中
-    #     """
-    #     logger.inf("bid_task.BidTask.restart")
-    #     queue = deep_get(self.settings, "task.queue")
-    #     complete = deep_get(self.settings, "task.complete")
-    #     queue += complete
-    #     deep_set(self.settings, "task.queue", queue)
-    #     logger.info(f"queue: {queue}")
-    #     self.init_state(self.settings)
+    def restart(self):
+        """ 将json中 complete 添加到 queue中
+        """
+        logger.inf("BidTask.restart")
+        queue = deep_get(self.settings, "stateQueue")
+        complete = deep_get(self.settings, "stateComplete")
+        queue += complete
+        deep_set(self.settings, "stateQueue", queue)
+        logger.info(f"queue: {queue}")
+        self.init_state(self.settings)
 
     def have_next_state(self):
-        if not deep_get(self.settings, "task.queue"):
+        if not deep_get(self.settings, "stateQueue"):
             return False
         return True
 
     def complete_task(self):
         """ 将json中 queue 头元素出队,添加到complete中
         """
-        queue = deep_get(self.settings, "task.queue")
-        complete = deep_get(self.settings, "task.complete")
+        queue = deep_get(self.settings, "stateQueue")
+        complete = deep_get(self.settings, "stateComplete")
         complete.append(queue.pop(0))
-        deep_set(self.settings, "task.queue", queue)
-        deep_set(self.settings, "task.complete", complete)
+        deep_set(self.settings, "stateQueue", queue)
+        deep_set(self.settings, "stateComplete", complete)
         logger.info(f"{self.state_idx} complete, queue: {queue}\n"
                     f"complete: {complete}")
         if queue:
@@ -276,10 +295,9 @@ class BidTask:
         # 解析 html_list_match 源码
         self.tag_list = self.web_brows.get_bs_tag_list()
         self.process_tag_list()
-        if self.state.state == "complete":
+        if self.State.state == "complete":
             self.complete_task()
             return "complete"
-        self.state.set_interrupt(self.list_url, self.bid)
         # need save json
         return "continue"
 
@@ -287,48 +305,47 @@ class BidTask:
         """ 遍历处理tag_list
         若能遍历到结尾,保存 interruptUrl 和 interrupt
         """
-        logger.hr("bid_task.BidTask.process_tag_list", 3)
-        # self.bid_list = ()
+        logger.hr("BidTask.process_tag_list", 3)
         for idx, tag in enumerate(self.tag_list):
             try:
                 self.bid.receive(*self.bid_tag.get(tag))
-                logger.debug(str(self.bid.message))
+                # logger.debug(str(self.bid.message))  # 打印每次获得的项目信息
             except Exception:
                 logger.error(f"idx: {idx} tag error: {tag}\n"
                              f"{traceback.format_exc()}")
                 continue
 
-            if self.state.bid_is_end(self.bid):
-                logger.info("bid is end")
-                self.state.complete()  # set self.state.state = "complete"
+            if self.State.bid_is_end(self.bid):
+                self.State.complete()  # set self.State.state = "complete"
                 break
-            if not self.state.newest:
+            if not self.State.newest:
                 # 只执行一次,设置state为interrupt, 保存第一个项目信息到 newest
-                self.state.save_newest_and_interrupt(self.bid)
-            if not self.state.start:
-                self.state.bid_is_start(self.bid)
-                if not self.state.start:
+                self.State.save_newest_and_interrupt(self.bid)
+            if not self.State.start:
+                if not self.State.bid_is_start(self.bid):
                     continue
-            self.process_bid(self.bid)
+
+            self.State.set_interrupt(self.list_url, self.bid)
+            self.list_file.write(f"{str(self.bid.message)}\n")
+            self.title_trie_search(self.bid)
         logger.info(f"tag stop at {idx}")
 
-    def process_bid(self, bid_prj: Bid):
-        """
+    def title_trie_search(self, bid_prj: Bid):
+        """ 处理 bid对象
+
         Args:
             bid_prj (bid_web_brows.Bid): 保存 bid 信息的对象
         """
-        self.list_file.write(f"{str(bid_prj.message)}\n")
         result: list = title_trie.search_all(bid_prj.name)
         if result:
             logger.info(f"{result} {self.bid.message}")
             result.append(bid_prj.message)
             self.match_list_file.write(f"{str(result)}\n")
-        # self.bid_list += (bid_prj.message,)
 
     def open_list_url(self, url, reOpen=0):
         """ 封装web_brows行为,打开浏览页面，获得裁剪后的页面源码
         """
-        logger.hr("bid_task.BidTask.open_list_url", 3)
+        logger.hr("BidTask.open_list_url", 3)
         try:  # 在打开网页后立刻判断网页源码是否符合要求
             self.web_brows.open(url=url)
             self.web_brows.cut_html()
@@ -382,11 +399,7 @@ def _bid_to_dict(bid_prj=None):
             "url": bid_prj.url
         }
     else:
-        return {
-            "name": "",
-            "date": "",
-            "url": ""
-        }
+        return {key: "" for key in ("name", "date", "url")}
 
 
 class TaskManager:
@@ -402,7 +415,7 @@ class TaskManager:
             save (bool): True: 是否保存到json中
             creat_new (bool): True: 保存到新的配置文件，默认为False
         """
-        logger.hr("bid_task.init", 3)
+        logger.hr("TaskManager.__init__", 3)
         self.task_dict = {}
         self.settings = read_json(json_file)  # json文件内容
         self.json_file = json_file  # json文件路径
@@ -418,14 +431,14 @@ class TaskManager:
         Args:
             new_name (str): 设置新开始的网站任务 必须在task.queue中
         """
-        logger.hr("bid_task.build_new_task", 3)
+        logger.hr("TaskManager.new_task", 3)
         self._return_task_name(new_name)
         deep_set(self.settings, "task.run_time", date_now_s())  # 写入运行时间
-        self.build_task()
+        self._build_task()
         save_json(self.settings, self.json_file)
 
-    def build_task(self):
-        logger.info("bid_task.TaskManager.build_task")
+    def _build_task(self):
+        logger.info("TaskManager._build_task")
         if self.task_name not in self.task_dict:
             self.task_dict[self.task_name] = \
                 BidTask(self.settings[self.task_name], self.task_name)
@@ -451,22 +464,25 @@ class TaskManager:
     def task_run(self):
         """ 获得任务, 初始化 state
         """
-        logger.hr("bid_task.task_run", 3)
+        logger.hr("TaskManager.task_run", 3)
         task: BidTask = self.task_dict[self.task_name]
+        if task.settings["stateQueue"] == []:
+            task.restart()  # TODO
         while task.init_state():  # 若 queue中还有state
             state = ""
             while state != "complete": 
                 state = task.get_url_list()  # 继续state任务
                 save_json(self.settings, self.json_file)
-                logger.info("sleep 3s , you can stop now")
-                for t in range(1, 4):
-                    logger.info(f"sleep {t}s now")
-                    sleep(1)  # TODO 后期换成定时器
+                print("sleep 3s , you can stop now")
+                if state != "complete":
+                    for t in range(1, 4):
+                        print(f"sleep {t}s now")
+                        sleep(1)  # TODO 后期换成定时器
         logger.info(f"task {self.task_name} is complete")
         return "complete"
         
     def task_complete(self):
-        logger.hr("bid_task.task_complete", 3)
+        logger.hr("TaskManager.task_complete", 3)
         self.queue.append(self.queue.pop(0))
         deep_set(self.settings, "task.queue", self.queue)
         save_json(self.settings, self.json_file)
