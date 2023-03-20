394f9d7a7cd0e4544dd6f331660df318842a4211
weeswelai <52352067+weeswelai@users.noreply.github.com>
2023-03-20 11:36:23 +0800

    reactor(lineAddLiTag): 将网址变为<a>标签

--- test/lineAddLiTag.py
+++ test/lineAddLiTag.py
@@ -1,4 +1,6 @@
 import datetime as dt
+import json
+from module.utils import jsdump
 
 def date_now_s(file_new=False):
     """ 返回当前日期
@@ -18,19 +20,43 @@ def txt_to_html(file, html):
                 if line[0] != "[":
                     html_f.write(f"<li> {line.strip()} <li>\n")
                     continue
-                    
-                html_f.write(f"<li> {str(idx)}. {line.strip()} <li>\n")
+                line_l = line.replace("\'","").replace("[","").replace("]","").split(",")
+                html_t1 = line_l[0]
+                html_a = f'<a href="{line_l[-2]}">{line_l[-4]}</a>'
+                html_t2 = f"{line_l[-3]}, {line_l[-1]}"
+                html_f.write(f"<li> {str(idx)}. [{html_t1}]: {html_a} ,{html_t2}<li>\n")
                 idx += 1
 
+# def json_to_html(json_data=None, html=None, file=False):
+#     if file:
+#         with open (json_data, "r", encoding="utf-8") as f:
+#             json_data = json.loads(f.read())
+#     with open(html, "w", encoding="utf-8") as f:
 
-file = r"./data/bid_list_hkgy.txt"
-match_file = r"./data/bid_match_list_hkgy.txt"
+# ["jdcg", "zzlh", "hkgy", "zhzb", "qjc"]
+task_name = ["jdcg", "zzlh", "hkgy", "zhzb", "qjc"]
+file = f"./data/bid_list_{task_name}.txt"  # ./data/bid_match_list_.txt
+file = ""
+match_file = f"./data/bid_match_list_{task_name}.txt"  # ./data/bid_match_list_.txt
+json_read = "./data/api front list cggg list LMID=1149231276155707394&pageNo=1&purchaseType=公开招标&_t=1671776534822_2022_12_23-14_28_22_test.html"
+json_file = f""
+list_html = f'.{"".join(file.split(".")[: -1])}{date_now_s(True)}.htm'
+match_html = f'.{"".join(match_file.split(".")[: -1])}{date_now_s(True)}.htm'
 
-list_html = f'.{"".join(file.split(".")[: -1])}{date_now_s(True)}.html'
-match_html = f'.{"".join(match_file.split(".")[: -1])}{date_now_s(True)}.html'
 
 if file:
     txt_to_html(file, list_html)
 
-if match_file:
+if isinstance(task_name, str) and match_file:
     txt_to_html(match_file, match_html)
+elif isinstance(task_name, list):
+    for name in task_name:
+        match_file = f"./data/bid_match_list_{name}.txt"  # ./data/bid_match_list_{name}.txt  ./data/bid_list_{name}.txt
+        match_html = f'.{"".join(match_file.split(".")[: -1])}{date_now_s(True)}.htm'
+        txt_to_html(match_file, match_html)
+
+if json_file:
+    with open(json_read, "r", encoding="utf-8") as f:
+        json_data = json.loads(f.read())
+    with open(json_file, "w", encoding="utf-8") as f:
+        f.write(jsdump(json_data))
