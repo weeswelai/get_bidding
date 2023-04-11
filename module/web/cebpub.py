3fbc4dfd417cf845debf3d12633194746866157c
weeswelai <52352067+weeswelai@users.noreply.github.com>
2023-04-11 09:09:10 +0800

    fix: add cebpub class

---
+++ module/web/cebpub.py
@@ -0,0 +1,15 @@
+"""
+通用模板
+"""
+
+import module.web_brows as web_brows
+from module.log import logger
+from module.utils import *
+
+
+class Bid(web_brows.BidBase):
+    pass
+
+
+class Brows(web_brows.DefaultWebBrows):
+    pass
