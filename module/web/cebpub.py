d50df43ccf67b06bea47ce46306c6dd59d22cc8a
weeswelai <52352067+weeswelai@users.noreply.github.com>
2023-04-21 10:15:55 +0800

    feat: 增加cebpub的url searchDate参数

--- module/web/cebpub.py
+++ module/web/cebpub.py
@@ -14,5 +14,10 @@ class Bid(web_brows.BidBase):
 class Brows(web_brows.DefaultWebBrows):
     pass
 
-    def open_url(self, timeout=180):
-        return super().open_url(timeout)
+    def _open_url(self, timeout=180):
+        return super()._open_url(timeout)
+
+    def url_extra(self, url, *argv, **kwargv):
+        # 暂时不知道规律，先用1998年加上今日日期
+        date = date_days(0, "day")[-5:]
+        return f"{url}&searchDate=1998-{date}"
