bdcae7e6296dc9d049049f9fad6ed76baf033a23
weeswelai <52352067+weeswelai@users.noreply.github.com>
2023-05-06 16:20:16 +0800

    Dev (#70)
    * refactor: time逻辑
    
    * refactor: refacor queue insert
    
    * feat: title trie add new word
    
    * rafactor: 删除 self.task

--- module/task_manager.py
+++ module/task_manager.py
@@ -1,10 +1,8 @@
 """
 
 """
-
-# import asyncio
 import traceback
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from module import config
 from module.log import logger
@@ -38,49 +36,43 @@ class TaskNode:
         
 
 class TaskQueue:
-    head: TaskNode = None
+    head = None
     len = 0
 
     def __init__(self) -> None:
         for t in config.taskList:
             self.insert(t)
-            self.len += 1
 
-    def insert(self, task):
+    def insert(self, task) -> None:
         if not isinstance(task, TaskNode):
             task = TaskNode(task)
 
-        if self.is_empty():
+        node = self.head
+        if not node:  # empty queue
             self.head = task
-            return
+        else:
+            if task.nextRunTime <= node.nextRunTime:  # insert before first
+                self._insert_first(task)
+            else:
+                self._insert(task)
+        self.len += 1
 
-        fast = self.head
-        idx = 0
+    def _insert(self, task:TaskNode):
+        node = self.head
         while 1:
-            if task.nextRunTime <= fast.nextRunTime:
-                # 插在前面
-                if idx == 0:
-                    self._insert_first(task)
-                    break
-                slow.next = task
-                task.next = fast
+            if node.next is None:
+                node.next = task
                 break
-                
-            if fast.next is None:
-                fast.next = task
+            if  task.nextRunTime <= node.next.nextRunTime:
+                task.next = node.next
+                node.next = task
                 break
             else:
-                idx += 1
-                slow = fast
-                fast: TaskNode = fast.next
+                node: TaskNode = node.next
 
     def is_empty(self):
         if self.head is None and self.len == 0:
-            return True
-        return False
-
-    def len_is_one(self):
-        if self.len == 1:
+            logger.info("queue is empty")
             return True
         return False
 
@@ -90,6 +82,8 @@ class TaskQueue:
         task.next = next
 
     def print(self):
+        if self.is_empty():
+            logger.info(f"queue is empty")
         q = self.head
         while 1:
             logger.info(f"{q.name}: {time2str(q.nextRunTime)}")
@@ -98,6 +92,8 @@ class TaskQueue:
                 break
 
     def pop(self):
+        if self.is_empty():
+            return None
         q = self.head
         self.head = q.next
         q.next = None
@@ -105,6 +101,8 @@ class TaskQueue:
         return q
 
     def first_runtime(self):
+        if self.is_empty():
+            return None
         return self.head.nextRunTime
 
 
@@ -126,54 +124,53 @@ class TaskManager:
         deep_set(config, "task.run_time", date_now_s())  # 写入运行时间
 
     # TODO 写得很*, 重写
-    def task_run(self):
+    def task_run(self, task: BidTask):
         """ 完成一个任务
         """
-        logger.hr(f"task_run {self.task.name}", 1)
-        delay_range = self.task.delay if self.task.delay else NEXT_OPEN_DELAY
-        if self.task.page_list.queue_is_empty():  # 若为空,重新写入PageQueue
-            self.task.page_list.restart()
-        self.task.task_end = False
+        logger.hr(f"task_run {task.name}", 1)
+        delay_range = task.delay if task.delay else NEXT_OPEN_DELAY  # 移到task.py
+        if task.page_list.queue_is_empty():  # 若为空,重新写入PageQueue
+            task.page_list.restart()
+        task.task_end = False
 
-        if not self.task.txt.file_open:
-            self.task.txt.data_file_open()
+        if not task.txt.file_open:
+            task.txt.data_file_open()
 
-        logger.info(f"task PageQueue: {self.task.settings['PageQueue']}")
+        logger.info(f"task PageQueue: {task.settings['PageQueue']}")
         try:
             result = True
-            while self.task.init_state():  # task 按PageQueue顺序完成state
+            while task.init_state():  # task 按PageQueue顺序完成state
                 self.web_break()
-                result = self.url_task_run(delay_range)
+                result = self.url_task_run(task, delay_range)
         except WebTooManyVisits:
             result = False
-            deep_set(self.task.settings, f"{self.task.url_task}.error", True)
+            deep_set(task.settings, f"{task.url_task}.error", True)
 
         # 判断结果 计算下次运行时间, 返回 True 则 延迟 COMPLETE_DELAY , 错误则延迟10分钟或json设置里的时间        
         if result:
             delay = COMPLETE_DELAY
         else:
-            delay = self.task.error_delay if self.task.error_delay \
+            delay = task.error_delay if task.error_delay \
                 else ERROR_DELAY
             logger.warning(f"open_list_url_error, delay {delay}")
         nextRunTime = get_time_add(delay=delay)
         # 设置下次运行时间
-        deep_set(self.task.settings, "nextRunTime", nextRunTime)
-        self.task.nextRunTime = nextRunTime
+        deep_set(task.settings, "nextRunTime", nextRunTime)
         config.save()
-        self.task.txt.data_file_exit()
-        logger.info(f"task {self.task.name} " f"next run time: {nextRunTime}")
+        task.txt.data_file_exit()
+        logger.info(f"task {task.name} " f"next run time: {nextRunTime}")
         return nextRunTime
 
-    def url_task_run(self, delay_range):
+    def url_task_run(self, task: BidTask, delay_range):
         """完成一个state"""
-        logger.hr(f"{self.task.url_task}.url_task_run", 2)
+        logger.hr(f"{task.url_task}.url_task_run", 2)
         while 1:
             self.web_break()
             try:
-                result = self.task.process_next_list_web()
+                result = task.process_next_list_web()
                 self.web_break()
             except AssertionError:  # from task.BidTask._open_list_url
-                self.task.set_error_state()  # 设置state.error为True, 将当前state移动到PageWait
+                task.set_error_state()  # 设置state.error为True, 将当前state移动到PageWait
                 logger.error(f"{traceback.format_exc()}")
                 # TODO 这里需要一个文件保存额外错误日志以记录当前出错的网址, 以及上个成功打开的列表的最后一个项目
                 return False
@@ -181,7 +178,7 @@ class TaskManager:
             if result:
                 sleep_random(delay_range, message=" you can use 'Ctrl  C' stop now")
             else:
-                logger.info(f"{self.task.name} {self.task.url_task} is complete")
+                logger.info(f"{task.name} {task.url_task} is complete")
                 return True
 
     def web_break(self):
@@ -207,10 +204,10 @@ class TaskManager:
             logger.info(f"json: task.list is {config.taskList}")
             raise WebBreak
         while 1:
-            # 判断 TimerQueue第一个任务是否可执行
+            # 判断第一个任务是否可执行
             if self.next_task_ready():
-                task: TaskNode = queue.pop()    # 第一个任务出队
-                self.task = BidTask(config[task.name], task.name)
+                taskNode: TaskNode = queue.pop()    # 第一个任务出队
+                task = BidTask(config[taskNode.name], taskNode.name)
             else:
                 # 阻塞sleep定时
                 config.save()
@@ -220,8 +217,8 @@ class TaskManager:
             # 任务执行
             self.web_break()
             self.sleep_now = False
-            task.nextRunTime = str2time(self.task_run())  # 运行单个任务
-            queue.insert(task)  # 将任务插回队列中 
+            taskNode.nextRunTime = str2time(self.task_run(task))  # 运行单个任务
+            queue.insert(taskNode)  # 将任务插回队列中 
             queue.print()
 
 
@@ -229,6 +226,8 @@ class TaskManager:
         """阻塞的定时器,阻塞间隔为5秒"""
         
         time_sleep = (nextRunTime - datetime.now()).total_seconds() + 1
+        if time_sleep <= 0:
+            return
         logger.info(f"sleep {time_sleep}")
         interval = 5
         while 1:
@@ -248,28 +247,19 @@ class TaskManager:
         task: TaskNode = queue.head
         logger.info(f"first task {task.name} nextRunTime: {task.nextRunTime}")
         now = datetime.now().replace(microsecond=0)
-        print(time2str(now))
         # 若不处于 当天08时到22时的区间内, 将时间延迟至第二天09点 或当天9点
-        nextRunTime = during_runtime(time2str(now))
+        nextRunTime = during_runtime(now)
         if nextRunTime:
-            task.nextRunTime = str2time(nextRunTime)
-            deep_set(config, f"{task.name}.nextRunTime", nextRunTime)
+            task.nextRunTime = nextRunTime
+            deep_set(config, f"{task.name}.nextRunTime", str(nextRunTime))
             logger.info(f"set {task.name} nextRunTime {nextRunTime}")
             queue.insert(queue.pop())
-            # queue.print()
-            print("False 1")
             return False
         else:
             if task.nextRunTime <= now:
-                print("True 1")
                 return True
             else:
-                print("False 2")
                 return False
-
-    def _set_delay_range(self):
-        delay_range = self.task.delay if self.task.delay else NEXT_OPEN_DELAY
-        return delay_range
 
 
 bidTaskManager = TaskManager()
@@ -278,6 +268,9 @@ queue = TaskQueue()
 if __name__ == "__main__":
     try:
         bidTaskManager.loop()
+    except KeyboardInterrupt:
+        pass
     except Exception:
+        logger.error(traceback.format_exc())
+    finally:
         bidTaskManager.exit()
-        config.save()
