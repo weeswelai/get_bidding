ebecad4b2819f847fac5d65530cbfa556c4cffe6
weeswelai <52352067+weeswelai@users.noreply.github.com>
2023-06-13 11:43:08 +0800

    feat: add command restart

--- bid_run.py
+++ bid_run.py
@@ -1,22 +1,20 @@
 """
-启动任务
-读取配置, 初始化运行环境
-初始化 任务调度对象，网页浏览对象， 读取配置文件，将配置文件和网页浏览对象交给任务调度对象
-任务调度模块
-功能为
- 打开 .json配置文件
- 判断时间，判断访问频率是否合适
- 调度网页的打开、项目列表页面的翻页
+
 """
 import traceback
+from sys import argv
 
 from module.log import logger
 from module.task_manager import TaskManager
 
 bidTaskManager = TaskManager()
 
-def main():
+def main(argv: list):
+    argv = argv[1:] if len(argv) > 1 else argv
     try:
+        if argv[0] == "-r":
+            logger.hr("task restart", 3)
+            bidTaskManager.restart = True
         bidTaskManager.loop()
     except KeyboardInterrupt:
         pass
@@ -27,5 +25,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-            
+    main(argv)
