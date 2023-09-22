f527c72f8984b9632480acd0da899933cad28f9c
weeswelai <52352067+weeswelai@users.noreply.github.com>
2023-09-22 14:22:24 +0800

    Feat: add clash method

--- module/clash.py
+++ module/clash.py
@@ -1,5 +1,12 @@
 from get_url import HEADERS, TIMEOUT
+
 from module.get_url import RequestBase
+
+
+"""
+伪代码
+"""
+
 
 
 class Clash(RequestBase):
@@ -9,9 +16,41 @@ class Clash(RequestBase):
         self.headers = {"Authorization": f"Bearer {config['secret']}"}
         self.proxies_key = ""
 
-    def get_proxies_dict(self, key=""):
-        key
+    @property
+    def proxies(self):
+        """
+        获得代理列表
+        """
         url = f"http://{self.controller}/proxies"
         self._get(url, headers=self.headers)
         return self._response
 
+    @property.setter
+    def proxies(self, proxies):
+        pass
+
+    def switch_next_proxies(self):
+        """
+        切换到下一个可用代理
+        """
+        pass
+
+    def close_current_connections(self):
+        """
+        close connection for previous proxies,
+        used before switch to new proxies
+        
+        """
+        pass
+
+    def check_rules(self):
+        """
+        寻找并添加对应网址的规则
+        """
+        pass
+
+    def switch_profiles(self):
+        """
+        切换节点
+        """
+        pass
